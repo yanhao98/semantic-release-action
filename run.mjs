85e76d3941a7b2b1488df0480ed1679b05585127
严浩 <me@yanhao.ren>
2024-09-30 16:22:01 +0800

    refactor: simplify semantic-release workflow and enhance debug output

--- run.mjs
+++ run.mjs
@@ -16,7 +16,8 @@ const result = await semanticRelease.default(
 
 if (result !== false) {
   console.debug(`result :>> `, result);
+  console.debug(`result.nextRelease.notes :>> `, result.nextRelease.notes);
   console.debug(`result.nextRelease.version :>> `, result.nextRelease.version);
   console.debug(`result.nextRelease.gitTag :>> `, result.nextRelease.gitTag);
-  console.debug(`result.nextRelease.notes :>> `, result.nextRelease.notes);
+  console.debug(`result.nextRelease.gitHead :>> `, result.nextRelease.gitHead);
 }
