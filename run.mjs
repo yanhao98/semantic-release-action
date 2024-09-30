0b626d04613e9d0ff3501aefb8ff830fc30259ba
严浩 <me@yanhao.ren>
2024-09-30 16:14:47 +0800

    fix: 1614

--- run.mjs
+++ run.mjs
@@ -18,4 +18,5 @@ if (result !== false) {
   console.debug(`result :>> `, result);
   console.debug(`result.nextRelease.version :>> `, result.nextRelease.version);
   console.debug(`result.nextRelease.gitTag :>> `, result.nextRelease.gitTag);
+  console.debug(`result.nextRelease.notes :>> `, result.nextRelease.notes);
 }
